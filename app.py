5e356d35a2515464e7666bd8949ec19fce513e15
Huzaifa Imran <51008597+huzaifa003@users.noreply.github.com>
2024-08-21 11:40:02 +0000

    Finished the flask wrapped for sd15

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ CORS(app)
 # In-memory database
 jobs = {}
 
-
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"pong": "pong"})
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
+    data = request.get_json()
+    data = data.get('request')
     prompts = data.get('prompt', ['Car', 'House', 'Cat'])
     negative_prompt = data.get('negative_prompt', 'Blurry, deformed, low quality, bad anatomy...')
     num_of_images = int(data.get('num_of_images', 1))
@@ -21,7 +24,7 @@ def predict():
     seed = int(data.get('seed', 0))
     sharpness = float(data.get('sharpness', 2.0))
     performance_selection = data.get("performance_selection", "Speed")
-    webhook_url = data.get('webhook_url')
+    webhook_url = data.get('webhook_url', 'https://obscure-fortnight-64qw6xjq5rgh45g4-5000.app.github.dev/webhook')
     job_id = str(uuid.uuid4())
     
     if not webhook_url:
@@ -50,18 +53,27 @@ def predict():
     return jsonify({'job_id': job_id}), 200
     
 def start_job(job_id, prompts, negative_prompt, num_of_images, loras, guidance_scale, seed, sharpness, performance_selection, webhook_url):
-    res = requests.post('https://api.cortex.cerebrium.ai/v4/p-bfb5eb26/sd15/predict', json={
-        'prompt': prompts,
-        'negative_prompt': negative_prompt,
-        'num_of_images': num_of_images,
-        'loras': loras,
-        'guidance_scale': guidance_scale,
-        'seed': seed,
-        'sharpness': sharpness,
-        'performance_selection': performance_selection,
-        'webhook_url': webhook_url,
-        'job_id': job_id,
-    })
+
+    json=                  {
+            "request":{
+                'prompt': prompts,
+                'negative_prompt': negative_prompt,
+                'num_of_images': num_of_images,
+                'loras': loras,
+                'guidance_scale': guidance_scale,
+                'seed': seed,
+                'sharpness': sharpness,
+                'performance_selection': performance_selection,
+                'webhook_url': webhook_url,
+                'job_id': job_id,
+            }
+        }
+    
+    res = requests.post('https://api.cortex.cerebrium.ai/v4/p-bfb5eb26/sd15/predict', headers={
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiJwLWJmYjVlYjI2IiwiaWF0IjoxNzI0MjE1ODgyLCJleHAiOjIwMzk3OTE4ODJ9.q_vaUwVr2bQ2ZEFo7r0VDa5j9xIvNFdxVe5ie8RnPO9I7lxaA-r6wba_CElkGFXmy34f55ncFxrS_r7koW3Fui7GAQXxOBXfuQnLPMX3uPdwQlvHsUYwQVZrBa1_7CZ5CRCLGT1Abl06uQOAE2FHHoo3obRRl4Gswjv7cjt2E5ZP6M0Zs1IFeduyduvBxe2ykTu5cLNok_vm5_aR2GOf2mSyFLUHMbkxCUCwuRcKDUw6qOmW9IQ9_OyYz4Urv-HPnrmf8lmCmfJaUHSMpr9XLa9wIzeHxdFh-IXeevCyAE7LsLpBGzLt-eX2_3blvQ1OVmnXi5qR3MHM9SL4cdb8Wg' 
+    }, json=json
+        
+    )
     
     if res.status_code != 200:
         jobs[job_id] = {
@@ -82,11 +94,14 @@ def start_job(job_id, prompts, negative_prompt, num_of_images, loras, guidance_s
     
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    response = data.get('response')
+    data = request.get_json()
+    print(data)
+    response = data.get('response')[0]
     job_id = response.get('job_id')
 
     jobs[job_id] = data
+
+    print(jobs[job_id])
     
     return jsonify({'message': 'Success'}), 200
 
